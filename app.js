e5b4e6f47eb0427d697259e95708e7abded507b1
Renda1234 <gsmtechnic@seznam.cz>
2021-04-07 13:30:42 +0200

    minot tweak

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-
-const inhalt = 
-`<div id="header">
+const inhalt =
+    `<div id="header">
 <h1>ToDo List</h1>
 <input type="text" class="Input" placeholder="Enter WhatToDo...">
 <span class="addBtn">Send it RFN!</span>
@@ -8,33 +7,32 @@ const inhalt =
 <ul class="myUL">
 `;
 
-document.body.innerHTML=inhalt;
+document.body.innerHTML = inhalt;
 
 function addNewTask(taskContent, status) {
-  this.taskContent=taskContent;
-  this.status='true';  
+    this.taskContent = taskContent;
+    this.status = 'true';
 }
 
-const tasks =[]; //globaly declared array
+const tasks = []; //globaly declared array
 
-document.querySelector('.addBtn').addEventListener('click',()=>{
-  let newTask=new addNewTask(document.querySelector('.Input').value,true);
-  tasks[tasks.length]=newTask;
-  showActiveTasks();
+document.querySelector('.addBtn').addEventListener('click', () => {
+    let newTask = new addNewTask(document.querySelector('.Input').value, true);
+    tasks[tasks.length] = newTask;
+    showActiveTasks();
 
 });
 
 
 
 
-let showActiveTasks = ()=>{
-  const taskNode = document.querySelector('.myUL');
-  const activeTasksArray = tasks.filter((option)=>option.status==='true');
-  taskNode.innerHTML=''; // i know it is not good, but its simple
-  activeTasksArray.forEach((option)=>{
-    let ToDoItem = document.createElement('li');
-    ToDoItem.textContent=option.taskContent;
-    taskNode.appendChild(ToDoItem);
-  })
+let showActiveTasks = () => {
+    const taskNode = document.querySelector('.myUL');
+    const activeTasksArray = tasks.filter((option) => option.status === 'true');
+    taskNode.innerHTML = ''; // i know it is not good, but its simple task
+    activeTasksArray.forEach((option) => {
+        let ToDoItem = document.createElement('li');
+        ToDoItem.textContent = option.taskContent;
+        taskNode.appendChild(ToDoItem);
+    })
 }
-
